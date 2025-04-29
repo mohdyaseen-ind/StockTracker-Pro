584601e1b9f2f32dec2ed3a1f2834d629cb6521f
mohdyaseen-ind <mohdyaseenind@gmail.com>
2025-04-29 09:38:57 +0530

    i

---
+++ .next/static/chunks/app_register_page_5c59699b.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_register_page_5c59699b.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_register_page_5c59699b.js",
+  "chunks": [
+    "static/chunks/_31614c23._.js"
+  ],
+  "source": "dynamic"
+});
